5ec3350a9d028ce149b75e7aa9ed49a04d08cffd
itrust1 <52253867+itrust1@users.noreply.github.com>
2020-10-08 15:16:21 +0530

    Update 17. Letter Combination of Phone Number.cpp

--- Backtracking/17. Letter Combination of Phone Number.cpp
+++ Backtracking/17. Letter Combination of Phone Number.cpp
@@ -1,3 +1,4 @@
+// Backtracking
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
@@ -31,5 +32,54 @@ public:
             backtrack(ind+1, digits, mp, s, result);
             s.pop_back();
         }
+    }
+};
+
+
+
+
+// Iterative 
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        if(digits.length()==0)
+            return result;
+        unordered_map<int,string> mp;
+        string alphas = "abcdefghijklmnopqrstuvwxyz";
+        int j = 0;
+        for(int i=2; i<=9; ++i){
+            if(i==7 || i==9){
+                mp[i] = alphas.substr(j,4);
+                j+=4;
+            }
+            else{
+                mp[i] = alphas.substr(j,3);
+                j+=3;
+            }
+        }
+        
+        queue<pair<string,int>> q;
+        for(auto x:mp[digits[0]-'0']){
+            string s = {};
+            s.push_back(x);
+            q.push(make_pair(s,1));
+        }
+        while(!q.empty()){
+            pair<string,int> p = q.front();
+            q.pop();
+            if(p.second==digits.length()){
+                result.push_back(p.first);
+            }
+            else{
+                for(auto x:mp[digits[p.second]-'0'] ){
+                    string s1 = p.first;
+                    s1.push_back(x);
+                    q.push(make_pair(s1,p.second+1));
+                }
+            }
+        }
+        return result;
+        
     }
 };
